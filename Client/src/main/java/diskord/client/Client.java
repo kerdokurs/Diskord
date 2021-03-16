8a1cd3a1e6ebf02f7fa7f2355ecf678da4a134d8
Kerdo Kurs <kerdokurs@gmail.com>
2021-03-16 17:25:22 +0200

    modules fix, dependency add

--- Client/src/main/java/diskord/client/Client.java
+++ Client/src/main/java/diskord/client/Client.java
@@ -1,11 +1,25 @@
 package diskord.client;
 
-public class Client {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lombok.Getter;
+
+import java.util.UUID;
+
+public class Client extends Application {
+  @Getter
+  private UUID uuid;
+
   public Client() {
     System.out.println("client has started");
   }
 
   public static void main(final String[] args) {
-    new Client();
+    launch(args);
+  }
+
+  @Override
+  public void start(final Stage stage) throws Exception {
+    stage.show();
   }
 }
--- Server/src/main/java/diskord/server/Server.java
+++ Server/src/main/java/diskord/server/Server.java
@@ -1,11 +1,19 @@
 package diskord.server;
 
-public class Server {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Server extends Application {
   public Server() {
     System.out.println("server has started");
   }
 
-  public static void main(String[] args) {
-    new Server();
+  public static void main(final String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(final Stage stage) throws Exception {
+    stage.show();
   }
 }
