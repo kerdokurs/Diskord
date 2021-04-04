79e2cb6504d7f73c4813b32eb6e62176537da5c6
Cardo <cardotisler@live.com>
2021-04-04 15:35:48 +0300

    credential verification

---
+++ Server/src/main/java/diskord/server/utils/CredentialVerifier.java
@@ -0,0 +1,12 @@
+package diskord.server.utils;
+
+import diskord.server.utils.credentials.CredentialError;
+
+public class CredentialVerifier {
+  public static CredentialError verify(String data){
+//    if(data == null){
+//      return CredentialError.NULL_ERROR;
+//    }
+//
+  }
+}
---
+++ Server/src/main/java/diskord/server/utils/credentials/CredentialError.java
@@ -0,0 +1,8 @@
+package diskord.server.utils.credentials;
+
+public enum CredentialError {
+  //username
+  LENGTH_ERROR,
+  NULL_ERROR,
+  INVALID_FORMAT_ERROR
+}
