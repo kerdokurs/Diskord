16fb69152f85b64388915154ffab95ebae7542ff
Kerdo Kurs <kerdokurs@gmail.com>
2021-04-04 17:02:10 +0300

    payload body constant fields for keying where data should be stored in the body

--- Server/src/main/java/diskord/server/payload/PayloadBody.java
+++ Server/src/main/java/diskord/server/payload/PayloadBody.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 public class PayloadBody extends HashMap<String, Object> implements Serializable {
-
+  // Key names where in the body should specific data be stored.
+  public static final transient String BODY_FIELD = "field";
+  public static final transient String BODY_TOKEN = "token";
+  public static final transient String BODY_MESSAGE = "message";
 }
 
 
