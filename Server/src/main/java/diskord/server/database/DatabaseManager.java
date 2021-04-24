1cbb59f6af6b281fab73db57ce0818b1d83c1f02
Kerdo Kurs <kerdokurs@gmail.com>
2021-04-24 13:50:59 +0300

    documentation of database manager

--- Server/src/main/java/diskord/server/database/DatabaseManager.java
+++ Server/src/main/java/diskord/server/database/DatabaseManager.java
@@ -14,27 +14,61 @@ public class DatabaseManager {
     factory = Persistence.createEntityManagerFactory("DiskordServer.database");
   }
 
-  public <T, D> T getOne(@NotNull Class<T> entityClass, @NotNull D primaryKey) {
+  public void close() {
+    factory.close();
+  }
+
+  public <T, D> T getOne(@NotNull final Class<T> entityClass, @NotNull final D primaryKey) {
     return runTransaction(em -> em.find(entityClass, primaryKey));
   }
 
-  public <T> List<T> getAll(Class<T> entityClass) {
+  /**
+   * Method for getting all entities by specified class.<p>
+   * <b>PS</b>. The table of the entity in the database must match its classname.
+   *
+   * @param entityClass class of entity
+   * @param <T>         type of entity
+   * @return list of entities
+   */
+  public <T> List<T> getAll(final Class<T> entityClass) {
+    return getAll(entityClass, entityClass.getSimpleName());
+  }
+
+  /**
+   * Method for getting all entities by specified class from specified table name.
+   * If table name matches the class name of the entity, DatabaseManager#getAll(Class)
+   * should be used.
+   *
+   * @param entityClass class of entity
+   * @param tableName   database table name of entity
+   * @param <T>         type of entity
+   * @return list of entities
+   * @see DatabaseManager#getAll(Class)
+   */
+  public <T> List<T> getAll(final Class<T> entityClass, final String tableName) {
     return runTransaction(em ->
-      em.createQuery(String.format("FROM %s", entityClass.getName()), entityClass)
+      em.createQuery(String.format("FROM %s", tableName), entityClass)
         .getResultList()
     );
   }
 
-  public <T> boolean save(@NotNull T obj) {
+  /**
+   * Method for saving an entity.
+   *
+   * @param obj entity to save
+   * @param <T> type of entity
+   * @return boolean whether the entity was saved
+   */
+  public <T> boolean save(@NotNull final T obj) {
     return runTransaction(em -> {
-      EntityTransaction et = em.getTransaction();
+      final EntityTransaction et = em.getTransaction();
 
       try {
         et.begin();
         em.persist(obj);
         et.commit();
-      } catch (Exception e) {
-        if (et != null) et.rollback();
+      } catch (final Exception e) {
+        et.rollback();
 
         return false;
       }
@@ -43,22 +77,67 @@ public class DatabaseManager {
     });
   }
 
-  public <T> boolean delete(@NotNull T obj) {
+  /**
+   * Method for deleting an entity by its reference.
+   *
+   * @param obj entity to delete
+   * @param <T> type of entity
+   * @return boolean whether the entity was deleted
+   */
+  public <T> boolean delete(@NotNull final T obj) {
     return runTransaction(em -> delete(obj, em));
   }
 
-  public <T> boolean delete(@NotNull T obj, EntityManager em) {
-    em.remove(obj);
+  /**
+   * Method for deleting entity by its reference. Only used inside database manager since
+   * is requires an entity manager as the second argument which is conveniently generated
+   * in DatabaseManager#runTransaction.
+   *
+   * @param obj entity to delete
+   * @param em  entity manager
+   * @param <T> type of entity
+   * @return boolean whether the entity was deleted
+   */
+  private <T> boolean delete(@NotNull final T obj, final EntityManager em) {
+    final EntityTransaction et = em.getTransaction();
+
+    try {
+      et.begin();
+      em.remove(obj);
+      et.commit();
+    } catch (final Exception e) {
+      et.rollback();
+
+      return false;
+    }
+
     return true;
   }
 
-  public <T, D> boolean deleteById(@NotNull Class<T> entityClass, @NotNull D id) {
+  /**
+   * Method for deleting entity with provided id.
+   *
+   * @param entityClass class of the entity to delete
+   * @param id          entity id
+   * @param <T>         type of entity
+   * @param <D>         type of entity primary key
+   * @return boolean whether the entity was deleted
+   */
+  public <T, D> boolean deleteById(@NotNull final Class<T> entityClass, @NotNull final D id) {
     return runTransaction(em -> {
       final T obj = getOne(entityClass, id);
       return delete(obj, em);
     });
   }
 
+  /**
+   * Method for running database transactions. Entity manager will be passed to the transaction
+   * as the only argument for easy use and will be closed after the transaction has been completed.
+   *
+   * @param transaction transaction to run
+   * @param <T>         type to run the transaction on
+   * @return result of the transaction
+   */
   public <T> T runTransaction(final Transaction<T> transaction) {
     final EntityManager em = factory.createEntityManager();
 
